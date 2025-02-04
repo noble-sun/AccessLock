32ece80ec7ce937d0b543d53c0340e0f7d9afd9f
noble-sun <matsubayashi.helio+github@gmail.com>
2025-02-04 15:56:20 -0300

    feat: list installed apps with a toggle function

--- app/src/main/java/com/example/accesslock/MainActivity.kt
+++ app/src/main/java/com/example/accesslock/MainActivity.kt
@@ -1,16 +1,43 @@
 package com.example.accesslock
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.accesslock.ui.theme.AccessLockTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +47,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             AccessLockTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+
+
+                    ListInstalledApps()
+
                 }
             }
         }
@@ -31,17 +62,80 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun ListInstalledApps() {
+      val context = LocalContext.current
+//    val pm = context.packageManager
+//    val appInfos = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//        pm.getInstalledApplications(PackageManager.ApplicationInfoFlags.of(0L))
+//    } else {
+//        pm.getInstalledApplications(0)
+//    }
+
+    // Right now this is magic to me, do not fully understand
+    val pm = context.packageManager
+    val mainIntent = Intent(Intent.ACTION_MAIN, null)
+    mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+    val resolvedInfos = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        pm.queryIntentActivities(
+            mainIntent,
+            PackageManager.ResolveInfoFlags.of(0L)
+        )
+    } else {
+        pm.queryIntentActivities(mainIntent, 0)
+    }
+
+    LazyColumn{
+        items(resolvedInfos) { app ->
+            var checked by remember { mutableStateOf(true)}
+
+            Card(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+                Row() {
+
+                    val bitmap = drawableToBitmap(app.activityInfo.loadIcon(pm)).asImageBitmap()
+                    Image(bitmap = bitmap, contentDescription = "app icon", modifier = Modifier.size(48.dp).padding(8.dp))
+                    Text(
+                        text = app.activityInfo.applicationInfo.loadLabel(pm).toString(),
+                        modifier = Modifier.padding(12.dp)
+                    )
+                    Switch(checked = checked, onCheckedChange = { checked = it })
+                }
+            }
+        }
+    }
+
+}
+
+//another magic that converts a drawable resource to a bitmap that can be used on the Image component
+fun drawableToBitmap(drawable: Drawable): Bitmap {
+    if (drawable is BitmapDrawable) {
+        return drawable.bitmap
+    }
+
+    val width = drawable.intrinsicWidth.coerceAtLeast(1)
+    val height = drawable.intrinsicHeight.coerceAtLeast(1)
+    val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    return bitmap
+}
+
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Green) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AccessLockTheme {
-        Greeting("Android")
+        Greeting("there")
     }
 }
